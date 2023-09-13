7c75e0088f1538b369586f688eedf92ee64fb251
Precious Adeyinka <pflash90@gmail.com>
2023-09-13 19:43:25 +0100

    refactor the login, dashboard, styling and layout for mobile optimization

---
+++ src/component/views/user/UserScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./UserScreen";
+
+const UserScreen = () => {
+	return <section></section>;
+};
+
+export default UserScreen;
